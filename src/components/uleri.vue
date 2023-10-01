ce396ca983edce43f1f87537eb6135140658cb52
Ulrika Eriksson <ulrika.m.a.eriksson@gmail.com>
2023-10-01 19:38:54 +0200

    created wireframe html

--- src/components/uleri.vue
+++ src/components/uleri.vue
@@ -6,7 +6,25 @@ defineProps({
     type: String,
     required: true
   }
+
+  //A PROP THAT TAKES IN THE SEARCH WORD?
 })
+
+function searchAlgoritm() {
+  //These two are only for me testing
+  let text = "A tasty mexican meal for the whole family!";
+  let textLower = text.toLowerCase();
+  let search = "mexican";
+  let searchLower = search.toLowerCase();
+
+  let result = textLower.includes(searchLower);
+
+  console.log(result);
+
+  //COMPARE EACH DESCRIPTION OF EACH RECIPE 
+  //IF TRUE, MAKE ARRAY? SEND TO AMANDAS RECIPE LIST?
+
+}
 </script>
 
 <template>
