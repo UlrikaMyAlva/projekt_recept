fa4803d978e26fc1d09317ca7789e6b5bf3d65ce
Wijdan Cederlid <wijdan.cederlid@iths.se>
2023-10-02 16:18:23 +0200

    wip created commentForm component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    <p>Nothing yet</p>
-  </div>
+  
+    <div>
+      <p>Nothing yet</p>
+      <CommentForm></CommentForm>
+    </div>
 </template>
 
 <script>
+import CommentForm from '../components/CommentForm.vue';
   export default {
-    
+    components: {
+    CommentForm
+},
+    data(){
+      return{
+        
+      }
+    }
   }
 </script>
 
